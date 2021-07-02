7b93ed123ea3162c42e45d0ec3adca81ceed0cee
unknown <sois.milindsir@gmail.com>
2021-07-02 22:22:08 +0530

    Chat app

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app)
+const port = process.env.PORT || 3000;
+
+http.listen(port, ()=>{
+    console.log(`listning on the port ${port}`)
+})
+app.use(express.static(__dirname+'/public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+// socket
+
+const io = require('socket.io')(http);
+io.on('connection', (socket)=> {
+    console.log('connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+})
